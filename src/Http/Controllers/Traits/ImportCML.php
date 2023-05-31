7704d3bc3edb8e61dcbd899d42cf00f912f16919
Timofey Tkachev <ttkacheff@gmail.com>
2023-05-31 16:00:31 +0300

    Refactoring requisites

--- src/Http/Controllers/Traits/ImportCML.php
+++ src/Http/Controllers/Traits/ImportCML.php
@@ -74,48 +74,56 @@ trait ImportCML{
             $category = $parseCML->getCollection('categories')->fetch();
             $dbCategory = new App\Models\Category();
             $dbCategory->createTree1c($category);
+            return $this->answer('success');
         }
 
         if ($fileType == "goods") {
             $products = $parseCML->getCollection('products');
             $dbProduct = new App\Models\Product();
             $dbProduct->createModel1c($products);
+            return $this->answer('success');
         }
 
         if ($fileType == "offers") {
             $offers = $parseCML->getCollection('products');
             $dbOffers = new App\Models\Offer();
             $dbOffers->createByMl($offers);
+            return $this->answer('success');
         }
 
         if ($fileType == "priceLists") {
             $priceType = $parseCML->getCollection('price_types');
             $dbPriceTipe = new App\Models\PriceType();
             $dbPriceTipe->createByMl($priceType);
+            return $this->answer('success');
         }
 
         if ($fileType == "prices") {
             $price = $parseCML->getCollection('offer_prices')->fetch();
             $dbPrice = new App\Models\Offer();
             $dbPrice->setPrice1c($price);
+            return $this->answer('success');
         }
 
         if ($fileType == "propertiesGoods") {
             $property = $parseCML->getCollection('properties_products');
             $dbProperty = new App\Models\Product();
             $dbProperty->importProperties1c($property);
+            return $this->answer('success');
         }
 
         if ($fileType == "propertiesOffers") {
             $specification = $parseCML->getCollection('properties_offers');
             $dbSpecification = new App\Models\Offer();
             $dbSpecification->importSpecifications1c($specification);
+            return $this->answer('success');
         }
 
         if ($fileType == "units") {
-            $category = $parseCML->getCollection('units')->fetch();
-            $dbCategory = new App\Models\Product();
-            $dbCategory->createByMl($category);
+            $requisite = $parseCML->getCollection('units')->fetch();
+            $dbRequisite = new App\Models\Product();
+            $dbRequisite->importRequisite1c($requisite);
+            return $this->answer('success');
         }
 
         /** @var Import $model */
